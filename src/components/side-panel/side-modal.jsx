bba658535966af428286cee05c2e5abc63470773
Sam Odras <odrassam@gmail.com>
2020-08-19 16:21:02 +0700

    Building Sidebar Interface

---
+++ src/components/side-panel/side-modal.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+import AddCircle from "@material-ui/icons/AddCircle";
+import IconButton from "@material-ui/core/IconButton";
+import TextField from "@material-ui/core/TextField";
+
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
+
+import { withStyles } from "@material-ui/core/styles";
+
+const DialogTextField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "black",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "black",
+    },
+    "& .MuiOutlinedInput-root": {
+      "&.Mui-focused fieldset": {
+        borderColor: "black",
+      },
+    },
+  },
+})(TextField);
+
+class AlertDialog extends React.Component {
+  state = {
+    open: false,
+  };
+  handleClickOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <IconButton
+          onClick={this.handleClickOpen}
+          className="divider-title__button"
+        >
+          <AddCircle className="divider-title__icon" />
+        </IconButton>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Add Channel ?</DialogTitle>
+          <DialogContent className="side-modal-form">
+            <DialogTextField
+              variant="outlined"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              label="Name of Channel"
+            />
+            <DialogTextField
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              label="About the Channel"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              startIcon={<CheckIcon />}
+              variant="outlined"
+              onClick={this.handleClose}
+              style={{ textTransform: "none" }}
+            >
+              Add
+            </Button>
+            <Button
+              startIcon={<CloseIcon />}
+              variant="outlined"
+              onClick={this.handleClose}
+              color="secondary"
+              style={{
+                textTransform: "none",
+                marginRight: "15px",
+                marginBottom: "5px",
+              }}
+            >
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AlertDialog;
--- src/components/side-panel/side-panel.component.jsx
+++ src/components/side-panel/side-panel.component.jsx
@@ -1,13 +1,98 @@
-import React from 'react';
-import './side-panel.styles.scss';
+import React from "react";
+import SidePanelLogo from "../../assets/logo.png";
+import "./side-panel.styles.scss";
+import AddChannelButton from './side-modal';
+
+import Avatar from "@material-ui/core/Avatar";
+import Tooltip from "@material-ui/core/Tooltip";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import CompareArrows from "@material-ui/icons/CompareArrows";
+import MessageIcon from "@material-ui/icons/Message";
+
 
 class SidePanel extends React.Component {
-  render(){
-    return(
-      <div className="sidepanel-container">
-        Sidepanel Container
-      </div>
-    )
+  state = {
+    anchorEl: null,
+  };
+  handleOpen = (e) => {
+    this.setState({
+      anchorEl: e.currentTarget,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      anchorEl: null,
+    });
+  };
+  render() {
+    const { anchorEl } = this.state;
+    return (
+      <React.Fragment>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem disabled onClick={this.handleClose}>
+            Signed as Bagus Ridho
+          </MenuItem>
+          <MenuItem selected onClick={this.handleClose}>Change Avatar</MenuItem>
+          <MenuItem onClick={this.handleClose}>Sign Out</MenuItem>
+        </Menu>
+        <div className="sidepanel-container">
+          <div className="sidepanel-header" onClick={this.handleOpen}>
+            <div className="sidepanel-header__logo">
+              <img
+                src={SidePanelLogo}
+                alt="Side panel logo"
+                style={{ width: "100%" }}
+              />
+            </div>
+            <Tooltip title="Click to see options" arrow placement="right">
+              <div className="sidepanel-header__avatar">
+                <Avatar className="avatar-image">B</Avatar>
+                <p>Bagus Ridho</p>
+                <ExpandMore />
+              </div>
+            </Tooltip>
+          </div>
+          <div className="sidepanel-content">
+            <div
+              className="sidepanel-content__channels"
+              style={{ marginBottom: "30px" }}
+            >
+              <div className="divider-title">
+                <CompareArrows className="divider-title__icon" />
+                <p>CHANNELS(2)</p>
+                <AddChannelButton/>
+              </div>
+              <div className="content-item">#React Channel</div>
+              <div className="content-item">#Vue Channel</div>
+              <div className="content-item">#Kotlin Channel</div>
+              <div className="content-item">#Android Channel</div>
+              <div className="content-item">#Php Channel</div>
+            </div>
+
+            <div className="sidepanel-content__users">
+              <div className="divider-title">
+                <MessageIcon className="divider-title__icon " />
+                <p>DIRRECT MESSAGES(2)</p>
+              </div>
+              <div className="content-item">@Charlie</div>
+              <div className="content-item">@Samodra</div>
+              <div className="content-item">@Hafiyan</div>
+              <div className="content-item">@Chirstine</div>
+              <div className="content-item">@Masduki</div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
